7f500cbfedf5521713432f33e2b5957217f1d2a0
Scott Lyons <scottalyons@gmail.com>
2022-06-15 18:28:58 -0700

    Initial code commit

---
+++ pre_commit_hooks/check_docusaurus_frontmatter.py
@@ -0,0 +1,33 @@
+import argparse
+import markdown
+from typing import Sequence
+
+
+def check_docusaurus_frontmatter(input_str) -> bool:
+    md = markdown.Markdown(extensions=['full_yaml_metadata'])
+    md.convert(input_str)
+    mandatory_keys = ("title", "id", "description", "sidebar_position", "tags")
+    for k in mandatory_keys:
+        if k not in md.Meta:
+            raise KeyError(f"missing '{k}'")
+
+    True
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filenames", nargs="*", help="Filenames to check")
+
+    args = parser.parse_args(argv)
+    retval = 0
+    for filename in args.filenames:
+        with open(filename, 'rb') as f:
+            try:
+                check_docusaurus_frontmatter(filename.read())
+            except KeyError as exc:
+                print(f"{filename}: {exc}")
+                retval = 1
+    
+    return retval
+
+if __name__ == "__main__":
+    raise SystemExit(main())
